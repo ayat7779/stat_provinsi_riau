42066b3a89c8c6150e4ba79c2e0e108dbb77cfb0
ayat7779 <ayat7779@yahoo.com>
2025-06-23 22:34:32 +0700

    memodifikasi tampilan style.css

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,9 @@
 <hr>
-        <footer>
-            <p>&copy; <?php echo date("Y"); ?> Aplikasi Manajemen Data Statistik Riau</p>
-        </footer>
-    </div>
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Aplikasi Manajemen Data Statistik Riau</p>
+</footer>
+</div>
+
 </body>
+
 </html>
